c029a97023ed4565bd7ab2580fba44f20ab8b825
arunpratapsingh17 <218apsj@gmail.com>
2020-03-27 12:19:27 +0530

    Code to find the given pair

---
+++ searching/2_pointer_approach_to_find_given_sum.java
@@ -0,0 +1,36 @@
+/*https://ide.geeksforgeeks.org/30981SspdV */
+
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
+		int flag=-1;
+		int l=0;
+		int r=n-1;
+		int x=sc.nextInt();
+		while(l<=r){
+		    if(arr[l]+arr[r]==x){
+		        flag=1;
+		        break;
+		    }
+		    if((arr[l]+arr[r])>x){
+		        r=r-1;
+		    }
+		    else {
+		        l=l+1;
+		    }
+		}
+		if(flag==-1){
+		    System.out.println("Not found");
+		}
+		else{
+		    System.out.println("Found");
+		}
+	}
+}
